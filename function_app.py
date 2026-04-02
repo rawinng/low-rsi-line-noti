06a2c9ec3d92e1eb6a9f09963297b8309ea41223
Rawin Ngamloet <ngamlraw@phatrasec.com>
2026-04-02 19:35:11 +0700

    chore: update time schedule to 21:00 bkk time

--- function_app.py
+++ function_app.py
@@ -4,8 +4,8 @@ from main import main
 
 app = func.FunctionApp()
 
-
-@app.timer_trigger(schedule="0 0 22 * * 1-5", arg_name="timer", run_on_startup=False)
+# This timezone is UTC. Adjust the schedule accordingly if you want to run it at a specific local time.
+@app.timer_trigger(schedule="0 0 14 * * 1-5", arg_name="timer", run_on_startup=False)
 def low_rsi_scanner(timer: func.TimerRequest) -> None:
     logging.info("low_rsi_scanner triggered")
     main()
